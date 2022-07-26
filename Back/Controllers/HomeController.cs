81f9acb8260c2a5df59a3cc552891a22adbcc0f5
elenaagudo <elenaagudo17@gmail.com>
2022-07-26 21:52:19 +0200

    Create shopping lists

--- Back/Controllers/HomeController.cs
+++ Back/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -19,12 +20,51 @@ namespace Back.Controllers
             db = context;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             // en la pagina de inicio se muestran las ultimas 5 listas de la compra
-            List<ShoppingList> lastShoppingLists = db.ShoppingList.Include(s => s.ListProducts).ThenInclude(s => s.Product).Where(s => !s.IsDeleted).OrderByDescending(s => s.CreateDate).Take(5).ToList();
+            List<ShoppingList> lastShoppingLists = db.ShoppingList.Include(s => s.ListProducts).ThenInclude(s => s.Product).Include(s => s.Supermarket).Where(s => !s.IsDeleted).OrderByDescending(s => s.CreateDate).Take(5).ToList();
 
             return View(lastShoppingLists);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            List<Supermarket> supermarketList = db.Supermarket.Where(s => s.IsDeleted == false).ToList();
+            Dictionary<string, string> supermarkets = new Dictionary<string, string>();
+
+            foreach (Supermarket mySupermarket in supermarketList)
+            {
+                supermarkets.Add(mySupermarket.Id.ToString(), mySupermarket.Name);
+            }
+
+            return Ok(supermarkets);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(int id)
+        {
+            ShoppingList myShoppingList = new ShoppingList();
+            myShoppingList.Supermarket = db.Supermarket.Find(id);
+            myShoppingList.Code = GenerateCode(myShoppingList.Supermarket.Name);
+            myShoppingList.CreateDate = DateTime.Now;
+            myShoppingList.DeleteDate = DateTime.MinValue;
+            myShoppingList.Active = true;
+            myShoppingList.Printed = false;
+            myShoppingList.IsDeleted = false;
+
+            db.ShoppingList.Add(myShoppingList);
+            db.SaveChanges();
+
+            return Ok(myShoppingList.Code);
+        }
+
+        private string GenerateCode(string supermarket)
+        {
+            string code = supermarket.Substring(0, 3).ToUpper() + DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            return code;
         }
     }
 }
